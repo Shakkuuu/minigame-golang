ed016ba38b2cbbf5e109239eeb74c6ed6e4b387c
Shakkuuu <shakku0521@gmail.com>
2023-01-17 14:41:06 +0900

    敵への攻撃

--- main.go
+++ main.go
@@ -17,16 +17,34 @@ func main() {
 	r.GET("/", index)
 	r.GET("minigame", minigamemain)
 
+	r.POST("dmgp", dmgplayer)
+	r.POST("dmge", dmgenemy)
+
 	// 8082ポートで起動
 	r.Run(":8084")
 }
 
 func index(c *gin.Context) {
+	p = minigame.UserStatus{HP: 10, ATK: 1}
+	e = minigame.EnemyStatus{Name: "スライム", HP: 5, ATK: 1}
 	c.HTML(200, "index.html", nil)
 }
 
+var p minigame.UserStatus
+var e minigame.EnemyStatus
+
 func minigamemain(c *gin.Context) {
-	p := minigame.UserStatus{HP: 10, ATK: 1}
-	e := minigame.EnemyStatus{Name: "スライム", HP: 5, ATK: 1}
-	c.HTML(200, "game.html", gin.H{"player": p, "suraimu": e})
+	player := p
+	enemy := e
+	c.HTML(200, "game.html", gin.H{"player": player, "suraimu": enemy})
+}
+
+func dmgplayer(c *gin.Context) {
+	p.HP -= e.ATK
+	c.Redirect(303, "/minigame")
+}
+
+func dmgenemy(c *gin.Context) {
+	e.HP -= p.ATK
+	c.Redirect(303, "minigame")
 }
